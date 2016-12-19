2f58d09e4eb1d432c173c732eda2109e35f106bc
BBOLDT86 <boldt.brad@gmail.com>
2016-12-19 00:18:47 -0500

    Update copyroms.sh

--- copyroms.sh
+++ copyroms.sh
@@ -2,3 +2,6 @@
 sudo mkdir /media/usb
 sudo chown -R pi:pi /media/usb
 sudo mount /dev/sda1 /media/usb -o uid=pi,gid=pi
+sudo cp -R /media/usb/retropie/roms/nes /home/pi/RetroPie/roms/nes
+sudo cp -R /media/usb/retropie/roms/SNES /home/pi/RetroPie/roms/snes
+sudo cp -R /media/usb/retropie/roms/Sega /home/pi/RetroPie/roms/genesis
