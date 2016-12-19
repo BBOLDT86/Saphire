47910bda2b28f982783997dbe6e4534bdea4641a
BBOLDT86 <boldt.brad@gmail.com>
2016-12-18 21:46:58 -0500

    Update Chrome_Netflix_pi.sh

--- Chrome_Netflix_pi.sh
+++ Chrome_Netflix_pi.sh
@@ -1,10 +1,8 @@
 !#/bin/sh
 mkdir chrome
 cd Chrome
-wget browser
-wget other
-wget chrome 
-
+wget -c https://launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/8621644/+files/chromium-browser_47.0.2526.106-0ubuntu0.15.04.1.1192_armhf.deb
+wget -c https://launchpad.net/~canonical-chromium-builds/+archive/ubuntu/stage/+build/8621644/+files/chromium-codecs-ffmpeg-extra_47.0.2526.106-0ubuntu0.15.04.1.1192_armhf.deb
 apt-get install `dpkg -I chromium-browser*.deb | grep " Depends" | sed "s/^ Depends: //" | sed "s/ ([^)]\+)//g" | sed "s/ | [^ ]\+//g" | sed "s/,//g"`
 dpkg -i chromium-browser*.deb
 
@@ -13,4 +11,19 @@ ln -s chromium-browser chrome-browser
 ln -s chromium-browser chrome
 cd -
 
-sudo rm chrome
+
+apt-get install alsa-utils
+modprobe snd_bcm2835
+amixer cset numid=3 2
+
+apt-get install xorg xinit matchbox
+
+wget https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_7077.134.0_daisy-skate_recovery_stable-channel_skate-mp.bin.zip
+unzip chromeos*.zip
+sudo apt-get install kpartx
+sudo kpartx -avs chromeos*.bin
+sudo mkdir -p /home/pi/chromeos
+sudo mount -t ext2 /dev/mapper/loop0p3 -o ro /home/pi/chromeos/
+cd /home/pi/chromeos/
+find ./ -type f -name "libwidevine*"
+sudo cp /home/pi/chromeos/libwidevine*.so /usr/lib/chromium-browser/
