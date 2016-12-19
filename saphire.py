35e8697daed8df682f465560b9c82454cdf375d2
BBOLDT86 <boldt.brad@gmail.com>
2016-12-19 00:23:00 -0500

    Update saphire.py

--- saphire.py
+++ saphire.py
@@ -26,7 +26,7 @@ menu_data = {
       'title': "Entertainment", 'type': MENU, 'subtitle': "Shows, Movies, games and More",
       'options': [
         { 'title': "Kodi", 'type': COMMAND, 'command': 'kodi-standalone' },
-        { 'title': "Retro Pie", 'type': COMMAND, 'command': 'emulationstation' },
+        { 'title': "Retro Pie - Type emulationstation at prompt", 'type': COMMAND, 'command': 'emulationstation' },
 	{
           'title': "Websites", 'type': MENU, 'subtitle': "This is a sub-sub menu example!",
           'options': [
